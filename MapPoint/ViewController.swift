1d900de288cc78e6894c1e452a6edc6fe371c4e8
Ioannis Karagogos <ikaragogos@gmail.com>
2017-04-28 17:02:27 +0300

    Initial Commit

---
+++ MapPoint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapPoint
+//
+//  Created by planB on 28/4/17.
+//  Copyright © 2017 Ioannis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
